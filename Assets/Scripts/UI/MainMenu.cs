37170a79a0dbb5501f4b4cb0803e9a9402a5c22e
DESKTOP-QUI421C\nream <nream08@hotmail.com>
2018-01-27 16:47:06 -0600

    Added application scenes. added UI for main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour {
+
+    static string mainMenuScene = "mainMenu";
+    static string setupScene = "setup";
+
+    public void StartButtonClicked()
+    {
+        SceneManager.LoadScene(setupScene);
+    }
+
+    public void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
