872fd7b70fe76413f6c4ceab18632e664136a916
DrewStreb <drewstreb@gmail.com>
2018-01-27 17:01:42 -0600

    Shaker functionality and sound changes

--- Assets/Scripts/Environment/Shakeable.cs
+++ Assets/Scripts/Environment/Shakeable.cs
@@ -3,18 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shakeable : MonoBehaviour {
+    
+    private Dictionary<string, Vector3> movers = new Dictionary<string, Vector3>();
+    private AudioSource audioSource;
+    private float jiggle = 0.0f;
+    [SerializeField]
+    private float jiggle_speed = 5.0f;
+    [SerializeField]
+    private float jiggle_factor = 5.0f;
+    private Vector3 start_scale;
 
-    public void OnTriggerEnter(Collider other)
+    public virtual void Start()
     {
-        EnvObjectSound();
-        EnvObjectAnimate();
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        EnvObjectSound();
-        EnvObjectAnimate();
+        audioSource = GetComponent<AudioSource>();
+        start_scale = transform.localScale;
     }
 
-    protected virtual void EnvObjectSound(){ }
-    protected virtual void EnvObjectAnimate() { }
+    public void OnTriggerStay(Collider other)
+    {
+        // if its not in the list make a list entry for it
+        //print( other.name );
+        if( !movers.ContainsKey( other.name ) )
+            movers[other.name] = other.transform.position;
+
+        // check to see if its changed.
+        if( movers[other.name] != other.transform.position )
+        {
+            movers[other.name] = other.transform.position;
+            print( "Shakeable::OnTriggerEnter");
+            EnvObjectSound();
+            EnvObjectAnimate();
+        }
+    }
+    
+    protected virtual void EnvObjectSound()
+    {
+        print( "TestShaker::Audio" + audioSource );
+        if (audioSource && !audioSource.isPlaying)
+            audioSource.PlayOneShot(audioSource.clip);
+    }
+    protected virtual void EnvObjectAnimate()
+    {
+        print( "TestShaker::Anim" );
+        jiggle += jiggle_speed * Time.deltaTime;
+        if( jiggle > 2.0f * Mathf.PI ) jiggle = 0.0f;
+        float jiggle_modulation = 1.0f + (Mathf.Sin(jiggle) / jiggle_factor);
+        transform.localScale = new Vector3( start_scale.x * jiggle_modulation,
+                                            start_scale.y,
+                                            start_scale.z * jiggle_modulation );
+        //transform.Rotate( 0.0f, 180.0f * Time.deltaTime, 0.0f );
+    }
 }
