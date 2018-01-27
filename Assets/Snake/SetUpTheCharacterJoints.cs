ce4eb670196f98726cee573ba89b6ccfda0b7d41
KyleIsSuperCool <kylereichhart@yahoo.com>
2018-01-27 14:07:20 -0600

    submitting snek stuff
    asdf

---
+++ Assets/Snake/SetUpTheCharacterJoints.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpTheCharacterJoints : MonoBehaviour {
+
+	Rigidbody ParentRBody;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject parent = gameObject;
+		GameObject child = parent.transform.GetChild(0).gameObject;
+		CharacterJoint ChildCJoint;
+		Rigidbody ChildRBody;
+
+		while(child != null)
+		{
+			ParentRBody = parent.GetComponent<Rigidbody>();
+
+			ChildRBody=child.AddComponent<Rigidbody>();
+			ChildCJoint=child.AddComponent<CharacterJoint>();
+
+			ChildCJoint.connectedBody = ParentRBody;
+			ChildRBody.constraints = ParentRBody.constraints;
+
+			ChildRBody.useGravity = false;
+
+			parent = child;
+
+			if(parent)
+				child = parent.transform.GetChild(0).gameObject;
+			else
+				break;
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Snake/SnakeGuy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeGuy : MonoBehaviour {
+
+	Rigidbody MyRigidBody;
+	Animator AnimationController;
+
+	Camera MainCamera;
+
+	float SpeedMultiplier;
+
+	// Use this for initialization
+	void Start () {
+		MyRigidBody = GetComponent<Rigidbody>();
+		AnimationController = GetComponent<Animator>();
+
+		MainCamera = GameObject.FindObjectOfType<Camera>();
+
+		SpeedMultiplier = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKey(KeyCode.W))
+		{
+			Move(MainCamera.transform.up, 1.0f * SpeedMultiplier);
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			Move(MainCamera.transform.right, 1.0f * SpeedMultiplier);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			Move(-MainCamera.transform.right, 1.0f * SpeedMultiplier);
+		}
+
+		if(Input.GetKey(KeyCode.S))
+		{
+			Move(-MainCamera.transform.up, 1.0f * SpeedMultiplier);
+		}
+	}
+
+	void Move(Vector3 Direction, float force)
+	{
+		MyRigidBody.AddForce(Direction * force);
+	}
+}
