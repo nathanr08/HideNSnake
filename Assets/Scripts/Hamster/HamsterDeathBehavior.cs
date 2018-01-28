de53475aca05a2674cdcc1148dbd8ad55f74ff66
DESKTOP-QUI421C\nream <nream08@hotmail.com>
2018-01-28 12:48:01 -0600

    added hamster death

--- Assets/Scripts/Hamster/HamsterDeathBehavior.cs
+++ Assets/Scripts/Hamster/HamsterDeathBehavior.cs
@@ -15,19 +15,18 @@ public class HamsterDeathBehavior : State {
             hamsterController.DeathAudio.Play();
         }
 
+        hamsterController.SetVisibility(true);
     }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        // check for ending of death animation and destroy this gameobject        
-
-        Destroy(controller.gameObject);
-	}
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    //
+	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        Destroy(controller.gameObject);
+    }
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
