73d31b0ff8fefac248e1b900467655697acc29b5
DESKTOP-QUI421C\nream <nream08@hotmail.com>
2018-01-28 14:57:32 -0600

    fixing menu stuff

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
@@ -9,6 +10,53 @@ public class MainMenu : MonoBehaviour {
 
     static string mainMenuScene = "mainMenu";
     static string setupScene = "setup";
+
+    public Button startGameButton;
+    public Button exitButton;
+
+    public Button highlightedButton;
+
+    public BaseControllable controllable;
+
+    public float selectTime = .5f;
+    public float selectTimer = 0.0f;
+
+    public void Start()
+    {
+        controllable = GetComponent<BaseControllable>();
+        highlightedButton = startGameButton;
+    }
+
+    public void Update()
+    {
+        if(selectTimer <= selectTime)
+        {
+            selectTimer += Time.deltaTime;
+        }
+
+        if (Input.GetAxis(controllable.InputHandles.VerticalAxis) != 0.0f &&
+            selectTimer >= selectTime)
+        {
+            selectTimer = 0.0f;
+            if(highlightedButton == startGameButton)
+            {
+                highlightedButton = exitButton;
+            }
+            else
+            {
+                highlightedButton = startGameButton;
+            }
+        }
+        else
+        {
+            if(Input.GetButtonDown(controllable.InputHandles.Submit))
+            {
+                highlightedButton.onClick.Invoke();
+            }
+        }
+
+        highlightedButton.Select();
+    }
 
     public void StartButtonClicked()
     {
